f9504fbc4cf8f494fc10787200f7208315c6519d
John Adeleke <89088298+gostlite@users.noreply.github.com>
2022-07-01 16:22:51 +0100

    Create main.py
    G

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) == 1:
+        print(f"{len(sys.argv)-1} arguments.")
+    elif len(sys.argv) == 2:
+        print(f"{len(sys.argv)-1} argument:")
+        for n in range(len(sys.argv)):
+            if n != 0:
+                print(f"{n}: {sys.argv[n]}")
+    else:
+        print(f"{len(sys.argv)-1} arguments:")
+        for n in range(len(sys.argv)):
+            if n != 0:
+                print(f"{n}: {sys.argv[n]}")
+   
+   
